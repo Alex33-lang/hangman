ab8f3a57f925ff09c75b875dc9edc12ea4fcb539
Alex33-lang <lotvinov99@bk.ru>
2023-11-19 15:47:38 +0400

    мой первый комментарий

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+
+ls = ['кот', 'собака', 'лист', 'яблоко']
+def hangman(word):
+    num = random.randint(0, len(word)-1)
+    ls = word[num]
+    wrong = 0
+    stages = ["",
+              "_____     ",
+              "|         ",
+              "|    |    ",
+              "|    0    ",
+              "|   /|\   ",
+              "|    |    ",
+              "|   / \   "
+              ]
+    rletters = list(ls)
+    board = ["_"] * len(word)
+    win = False
+    print("Добро пожаловать на казнь!")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Введите букву: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "_" not in board:
+            print("Вы выйграли! Было загаданное слово: ")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("Вы проиграли! Было загадано слово: {}.".format(word))
+            
+hangman(ls)
